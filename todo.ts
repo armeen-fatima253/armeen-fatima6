3ddeac9b0fbe3f6d4357747d04ff39c8c13c237b
armeen-fatima253 <armeenali253@gmail.com>
2024-06-27 19:41:11 +0100

    Add files via upload

---
+++ todo.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer"
+let todos=[];
+let condition=true;
+while(condition){
+let todoans=await inquirer.prompt(
+    [
+        {
+            name:"armeen",
+            message:"what do you want to add in your todos?",
+            type:"input",
+        },
+        {
+            name:"ali",
+            message:"do you want to add more?",
+            type:"confirm",
+            default:"false",
+        }
+    ]
+)
+todos.push(todoans.armeen);
+condition=todoans.adddmore;
+console.log(todos);
+}
